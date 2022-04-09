ce8b4f1c8299bccc486916a38e736babf5a0012a
ThieuVanHoang <thieuvanhoang.0015@gmail.com>
2022-04-09 11:08:22 +0700

    noti

---
+++ src/Components/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from 'antd';
+import '../shared/styles/main_styles/search.scss'
+
+
+export const ButtonCpn = () => {
+  return (
+    <div>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  )
+}
+
+export default ButtonCpn;
