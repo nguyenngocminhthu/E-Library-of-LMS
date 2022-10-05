4ac7112c9c44cb510c74d5d5d7430b55f6fe237f
hle208 <haingoc.le@dxc.com>
2022-10-05 18:30:55 +0700

    handle socket client

--- src/Views/Login/Login.tsx
+++ src/Views/Login/Login.tsx
@@ -1,5 +1,5 @@
 import { Button, Col, Form, Input, Row } from "antd";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { logIn } from "../../redux/reducers/auth.reducer";
@@ -8,17 +8,24 @@ import { ReactComponent as Account } from "../../shared/img/icon/account.svg";
 import logoLogin from "../../shared/img/icon/logo-second.svg";
 import { ReactComponent as Password } from "../../shared/img/icon/shield-keyhole-line.svg";
 import "./Login.scss";
+import { SocketContext } from '../../context/socket.context';
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
   const [form] = Form.useForm();
   const dispatch: AppDispatch = useDispatch();
   const navigate = useNavigate();
+  const client = useContext(SocketContext);
 
   const onFinish = (values: any) => {
     dispatch(logIn(values))
       .unwrap()
       .then((rs: any) => {
+        if (!client.socket.connected) {
+          client.socket.connect();
+        }
+        client.socket.emit("SEND_JOIN_REQUEST", rs.user.id);
+        console.log('emit socket', client.socket);
         if (rs.user.role === "teacher") {
           navigate("/teacher/home");
         } else if (rs.user.role === "student") {
@@ -31,6 +38,7 @@ const Login = () => {
         console.debug("e: ", e);
       });
   };
+
 
   return (
     <div className="login">
