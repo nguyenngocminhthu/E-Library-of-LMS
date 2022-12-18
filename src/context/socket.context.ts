94529cfec0d43bd449948e9d49497625f7f5bf97
hle208 <haingoc.le@dxc.com>
2022-12-18 13:59:51 +0700

    socket

--- src/context/socket.context.ts
+++ src/context/socket.context.ts
@@ -2,12 +2,7 @@ import React from 'react';
 import socketIOClient, { Socket } from 'socket.io-client';
 const ENDPOINT = process.env.REACT_APP_BASE_URL || 'http://localhost:4000';
 
-export let socket: any;
-// if (ENDPOINT === 'http://localhost:4000') {
-//   socket = socketIOClient(ENDPOINT);
-// } else {
-socket = socketIOClient(ENDPOINT, { transports: ['polling', 'websocket'] });
-// }
+export let socket = socketIOClient(ENDPOINT, { transports: ['websocket'] });
 
 let listUser: Array<any> = [];
 let statistical: any = {};
