63711651657e248d07393307e6bffd314000d36c
leehari-ute <nhai30928@gmail.com>
2022-12-24 23:42:08 +0700

    import file excel to add multiple users

---
+++ src/Views/Leadership/SystemSetting/ModalFileExcel.tsx
@@ -0,0 +1,132 @@
+import { message, Modal, Table } from "antd";
+import { cloneDeep } from "lodash";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { createUsers } from "../../../redux/reducers/user.reducer";
+import { AppDispatch } from "../../../redux/store";
+import "./SystemSetting.style.scss";
+
+interface IUser {
+  userName: string;
+  userCode: string;
+  phone: string;
+  gender: number;
+}
+
+export const ModalFileExcel = (props: any) => {
+  const dispatch: AppDispatch = useDispatch();
+  const { excelRows, isModalOpen, setIsModalOpen } = props;
+  const [page, setPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [dataSource, setDataSource] = useState([]);
+  const [bodyCreate, setBodyCreate] = useState<any>([]);
+
+  const columns = [
+    {
+      title: "STT",
+      dataIndex: "stt",
+      key: "stt",
+    },
+    {
+      title: "TEN",
+      dataIndex: "userName",
+      key: "userName",
+    },
+    {
+      title: "MS",
+      dataIndex: "userCode",
+      key: "userCode",
+    },
+    {
+      title: "SDT",
+      dataIndex: "phone",
+      key: "phone",
+    },
+    {
+      title: "GIOITINH",
+      dataIndex: "gender",
+      key: "gender",
+    },
+  ];
+
+  const handleOk = () => {
+    dispatch(createUsers(bodyCreate))
+      .unwrap()
+      .then((rs) => {
+        console.log("tao xong: ", rs);
+        setIsModalOpen(false);
+      });
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  useEffect(() => {
+    if (isModalOpen) {
+      let tempRows = cloneDeep(excelRows);
+      tempRows.shift();
+      const dataConverted = tempRows.map((item: any) => {
+        return {
+          key: item[0],
+          stt: item[0],
+          userName: item[1],
+          userCode: item[2].toString(),
+          phone: item[3].toString(),
+          gender: item[4],
+        };
+      });
+      setDataSource(dataConverted);
+    }
+  }, [isModalOpen]);
+
+  useEffect(() => {
+    console.log("dataConverted: ", dataSource);
+    if (dataSource.length) {
+      const temp: any = cloneDeep(dataSource).map((item: any) => {
+        delete item.key;
+        delete item.stt;
+        item.gender = item.gender === "Nam" ? 0 : 1;
+        return item;
+      });
+      setBodyCreate(temp);
+    }
+  }, [dataSource]);
+  return (
+    <Modal
+      title="Nội dung file"
+      open={isModalOpen}
+      onOk={handleOk}
+      onCancel={handleCancel}
+      width="40%"
+      className="cancel-form"
+      okText="Import"
+      cancelText="Huỷ"
+    >
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        pagination={{
+          defaultPageSize: 10,
+          current: page,
+          total: dataSource.length,
+          showTotal(total, range) {
+            return `Total ${total} items`;
+          },
+          onChange(page, pageSize) {
+            setPage(page);
+          },
+          pageSize,
+          pageSizeOptions: ["10", "15", "20", "25"],
+          showSizeChanger: true,
+          onShowSizeChange(current, size) {
+            setPageSize(size);
+            setPage(1);
+          },
+        }}
+        scroll={{
+          y: 600,
+        }}
+      />
+    </Modal>
+  );
+};
